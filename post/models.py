058fe86a7ace40d8e1c6e35602cf88e2d2232273
X00155628 Aaron O Cuileann <X00155628@computing.stu.it-tallaght.ie>
2020-02-19 15:21:20 +0000

    Adding posts, homepage

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
+    body = models.TextField(),
+    date = models.DateField(),
+
+    def __str__(self):
+        return self.author
+
+    
--- post/views.py
+++ post/views.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomeView(TemplateView):
+    model = Post
     template_name = 'home.html'
     
