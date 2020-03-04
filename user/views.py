5cf564b52e24131ea832df3a9f78035ca2e91094
X00152561 Piotr Wojciechowski <X00152561@COMPUTING.STU.IT-TALLAGHT.IE>
2020-03-04 16:45:58 +0000

    Fixed Signup

--- user/views.py
+++ user/views.py
@@ -15,13 +15,12 @@ CustomUser = get_user_model()
 class SignUpView(CreateView):
     model = CustomUser
     form_class = SignUpForm
-    success_url = reverse_lazy('profiles:profiles-create')
+    success_url = reverse_lazy('profiles:create_profile')
     template_name = 'signup.html'
 
     def form_valid(self, form):
         username = self.request.POST['username']
         password = self.request.POST['password1']
-        print(username, password)
         user = form.save()
         login(self.request, user)
         return HttpResponseRedirect(self.success_url)
