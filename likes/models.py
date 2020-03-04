1822073f354fb88808db7457e906158c10b4ae5c
X00155628 Aaron O Cuileann <X00155628@computing.stu.it-tallaght.ie>
2020-03-04 15:34:00 +0000

    adding like to post

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
+from posts.models import Post
 # Create your models here.
+
+User = get_user_model()
+
+class Like(models.Model):
+  post = models.ForeignKey(
+    Post,
+    default=1,
+    on_delete=models.CASCADE
+  )
+  owner = models.ForeignKey(
+    User,
+    default=1,
+    on_delete=models.CASCADE
+  )
+
+
+  def __str__(self, *args, **kwargs):
+    return self.post.posts
