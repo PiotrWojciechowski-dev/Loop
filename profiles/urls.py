2b66974a2211a440285e6aecd40720710f63f978
Sean o Riordain <x00154002@mytudublin.ie>
2020-03-05 01:43:26 +0000

    Mates Functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ app_name = 'profiles'
 urlpatterns = [
     path('create/', views.create_profile, name='create_profile'),
     path('detail/<str:username>', views.ProfileView.as_view(), name='profile_detail'),
+    path('<int:pk>/update/', views.ProfileUpdateView.as_view(), name='update_profile'),
 ]
